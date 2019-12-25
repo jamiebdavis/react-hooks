edca5c8246e19699db5eef98615f8e9a51ef1289
jamiebdavis <jamied1985@gmail.com>
2019-12-25 09:37:34 +0000

    React Context & Hooks Tutorial #4

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import {ThemeContext} from "../contexts/ThemeContext";
 
 class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        const {dark, light, isLightTheme} = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className="book-list">
+            <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
                 <ul>
-                    <li>The Bible</li>
-                    <li>1985</li>
-                    <li>Cook Book</li>
+                    <li style={{background: theme.ui}}>The Bible</li>
+                    <li style={{background: theme.ui}}>1985</li>
+                    <li style={{background: theme.ui}}>Cook Book</li>
                 </ul>
             </div>
         );
