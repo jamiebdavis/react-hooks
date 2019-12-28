813716cb21a89c3257404440d41b2e082a6f15b1
jamiebdavis <jamied1985@gmail.com>
2019-12-28 11:46:55 +0000

    React Context & Hooks Tutorial #9 - Intro to Hooks

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 
 function App() {
     return (
         <div className="App">
-            <ThemeContextProvider>
-                <Navbar />
-                <BookList />
-            </ThemeContextProvider>
+            <AuthContextProvider>
+                <ThemeContextProvider>
+                    <Navbar/>
+                    <BookList/>
+                    <ThemeToggle/>
+                </ThemeContextProvider>
+            </AuthContextProvider>
         </div>
     );
 }
