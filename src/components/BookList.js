232abd37efe7bfb522c466f203f9c1f3829c14d1
jamiebdavis <jamied1985@gmail.com>
2020-01-01 10:29:44 +0000

    added multiple context hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,18 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import {ThemeContext} from "../contexts/ThemeContext";
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const {dark, light, isLightTheme} = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-                <ul>
-                    <li style={{background: theme.ui}}>The Bible</li>
-                    <li style={{background: theme.ui}}>1985</li>
-                    <li style={{background: theme.ui}}>Cook Book</li>
-                </ul>
-            </div>
-        );
-    }
-}
+const BookList = () => {
+    const {dark, light, isLightTheme} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+            <ul>
+                <li style={{background: theme.ui}}>The Bible</li>
+                <li style={{background: theme.ui}}>1985</li>
+                <li style={{background: theme.ui}}>Cook Book</li>
+            </ul>
+        </div>
+    );
+};
 
 export default BookList;
